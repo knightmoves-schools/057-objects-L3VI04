19770a65d4b7d88626d62d6d6463a954ebb87cda
L3VI04 <ljenkins@anamosa.k12.ia.us>
2025-11-04 13:15:11 -0600

    Add school object and display information on page

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+// Create the school object with three properties
+const school = {
+  name: "Anamosa High",
+  averageClassSize: 25,
+  mascot: "Blue Raider"
+};
+
+// Assign formatted string to the innerHTML of the element with id "result"
+document.getElementById("result").innerHTML = 
+  `${school.name} has an average class size of ${school.averageClassSize} and their mascot is a ${school.mascot}`;
+
 
