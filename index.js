e15a730efa9bb9fe6f75561dc9f548f2e00f2c33
L3VI04 <ljenkins@anamosa.k12.ia.us>
2025-11-04 13:16:50 -0600

    Change averageClassSize to undefined in school object

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Create the school object with three properties
 const school = {
   name: "Anamosa High",
-  averageClassSize: 25,
+  averageClassSize: undefined,
   mascot: "Blue Raider"
 };
 
 // Assign formatted string to the innerHTML of the element with id "result"
 document.getElementById("result").innerHTML = 
   `${school.name} has an average class size of ${school.averageClassSize} and their mascot is a ${school.mascot}`;
+
 
 
